f136531191d2c9a172e65f70dedec90f7872afb9
Michael Ludwig <lhkbob@gmail.com>
2016-12-20 12:31:54 -0600

    Restructure exports to have namespaces.
    All functions are documented now as well.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-// Hand-written Promisified wrapper around generated ESI code.
+/**
+ * Hand-written Promisified wrapper around generated ESI code.
+* @module eve_swagger_interface
+ */
+
+/**
+ * A Bluebird JS Promise.
+ * @external Promise
+ * @see http://bluebirdjs.com
+ */
 
 const Promise = require('bluebird');
 const Moment = require('moment');
@@ -26,6 +35,7 @@ module.exports = function(datasource, baseURL) {
      * @param {String} accessToken Optional Eve SSO access token for 
      *   authentication 
      * @return A new Api instance
+     * @private
      */
     var newApi = function(apiCtor, accessToken) {
         var api;
@@ -46,6 +56,8 @@ module.exports = function(datasource, baseURL) {
     /**
      * Create a new object holding the default options/parameters for an ESI 
      * request. Currently this configures the data source to tranquility.
+     * @return {Object} The default options
+     * @private
      */
     var defaultOpts = function() {
         return { 'datasource': datasource };
@@ -72,6 +84,7 @@ module.exports = function(datasource, baseURL) {
      *   authentication, pass `undefined` if not needed
      * @param {Function} resolve Promise resolve handler
      * @param {Function} reject Promise error resolution handler
+     * @private
      */
     var getCachedRequest = function(apiCtor, functionName, args, accessToken, 
                                     resolve, reject) {
@@ -168,6 +181,7 @@ module.exports = function(datasource, baseURL) {
      * @param {String} accessToken Optional access token to use for 
      *   authentication, pass `undefined` if not needed
      * @return A new promise that resolves to the data returned by the request
+     * @private
      */
     var newRequest = function(apiCtor, functionName, args, accessToken) {
         return new Promise(function(resolve, reject) {
@@ -176,35 +190,196 @@ module.exports = function(datasource, baseURL) {
         });
     };
 
-    exports.getCorporation = function(id) {
-        return newRequest(ESI.CorporationApi, 
-                          'getCorporationsCorporationId', [id]);
-    };
+    /**
+     * A namespace container for the [corporation](https://esi.tech.ccp.is/latest/#/Corporation)
+     * ESI endpoints.
+     * @see https://esi.tech.ccp.is/latest/#/Corporation
+     * @namespace
+     */
+    exports.Corporation = {
+        /**
+         * Get a corporation's public info from the ESI endpoint. This makes 
+         * an HTTP GET request to [`corporations/{id}/](https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_corporation_id).
+         * The request is returned as an asynchronous Promise that resolves to 
+         * an object parsed from the response JSON model. An example value looks 
+         * like:
+         *
+         * ```
+         * {
+         *   alliance_id: 434243723,
+         *   ceo_id: 180548812,
+         *   corporation_name: "C C P",
+         *   member_count: 656,
+         *   ticker: "-CCP-"
+         * }
+         * ```
+         *
+         * @param {Integer} id The corporation id
+         * @return {external:Promise} A Promise that resolves to the response of
+         *   the request
+         * @see https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_corporation_id
+         * @esi_link CorporationApi.getCorporationsCorporationId
+         */
+        get: function(id) {
+            return newRequest(ESI.CorporationApi, 
+                              'getCorporationsCorporationId', [id]);
+        },
 
-    exports.getCorporationAllianceHistory = function(id) {
-        return newRequest(ESI.CorporationApi, 
-                          'getCorporationsCorporationIdAllianceHistory', [id]);
-    };
+        /**
+         * Get a corporation's alliance history from the ESI endpoint. This 
+         * makes an HTTP GET request to [`corporations/{id}/alliancehistory/](https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_corporation_id_alliancehistory).
+         * The request is returned as an asynchronous Promise that resolves to 
+         * an array parsed from the response JSON model. An example value looks 
+         * like:
+         *
+         * ```
+         * [
+         *   {
+         *     "alliance": {
+         *       "alliance_id": 99000006,
+         *       "is_deleted": false
+         *     },
+         *     "record_id": 23,
+         *     "start_date": "2016-10-25T14:46:00Z"
+         *   },
+         *   {
+         *     "record_id": 1,
+         *     "start_date": "2015-07-06T20:56:00Z"
+         *   }
+         * ]
+         * ```
+         *
+         * @param {Integer} id The corporation id
+         * @return {external:Promise} A Promise that resolves to the response of
+         *   the request
+         * @see https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_corporation_id_alliancehistory
+         * @esi_link CorporationApi.getCorporationsCorporationIdAllianceHistory
+         */
+        getAllianceHistory: function(id) {
+            return newRequest(ESI.CorporationApi, 
+                              'getCorporationsCorporationIdAllianceHistory', 
+                              [id]);
+        },
 
-    exports.getCorporationIcons = function(id) {
-        return newRequest(ESI.CorporationApi, 
-                          'getCorporationsCorporationIdIcons', [id]);
-    };
+        /**
+         * Get a corporation's icon URLs from the ESI endpoint. This makes 
+         * an HTTP GET request to [`corporations/{id}/icons/](https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_corporation_id_icons).
+         * The request is returned as an asynchronous Promise that resolves to 
+         * an object parsed from the response JSON model. An example value looks 
+         * like:
+         *
+         * ```
+         * {
+         *   "px128x128": "https://imageserver.eveonline.com/Corporation/1000010_128.png",
+         *   "px256x256": "https://imageserver.eveonline.com/Corporation/1000010_256.png",
+         *   "px64x64": "https://imageserver.eveonline.com/Corporation/1000010_64.png"
+         * }
+         * ```
+         *
+         * @param {Integer} id The corporation id
+         * @return {external:Promise} A Promise that resolves to the response of
+         *   the request
+         * @see https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_corporation_id_icons
+         * @esi_link CorporationApi.getCorporationsCorporationIdIcons
+         */
+        getIcons: function(id) {
+            return newRequest(ESI.CorporationApi, 
+                              'getCorporationsCorporationIdIcons', [id]);
+        },
 
-    exports.getCorporationMembers = function(id, accessToken) {
-        return newRequest(ESI.CorporationApi, 
-                          'getCorporationsCorporationIdMembers', 
-                          [id], accessToken);
-    };
+        /**
+         * Get a corporation's member list from the ESI endpoint. This 
+         * makes an HTTP GET request to [`corporations/{id}/members/](https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_corporation_id_members).
+         * The request is returned as an asynchronous Promise that resolves to 
+         * an array parsed from the response JSON model. An example value looks 
+         * like:
+         *
+         * ```
+         * [
+         *   {
+         *     "character_id": 90000001
+         *   },
+         *   {
+         *     "character_id": 90000002
+         *   }
+         * ]
+         * ```
+         *
+         * @param {Integer} id The corporation id
+         * @param {String} accessToken The SSO access token of a member of the
+         *   corporation, used to authenticate the request
+         * @return {external:Promise} A Promise that resolves to the response of
+         *   the request
+         * @see https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_corporation_id_members
+         * @esi_link CorporationApi.getCorporationsCorporationIdMembers
+         */
+        getMembers: function(id, accessToken) {
+            return newRequest(ESI.CorporationApi, 
+                              'getCorporationsCorporationIdMembers', 
+                              [id], accessToken);
+        },
 
-    exports.getCorporationRoles = function(id, accessToken) {
-        return newRequest(ESI.CorporationApi, 
-                             'getCorporationsCorporationIdRoles',
-                             [id], accessToken);
-    };
+        /**
+         * Get a corporation's member list with roles for each character from 
+         * the ESI endpoint. This makes an HTTP GET request to 
+         * [`corporations/{id}/members/](https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_corporation_id_roles).
+         * The request is returned as an asynchronous Promise that resolves to 
+         * an array parsed from the response JSON model. An example value looks 
+         * like:
+         *
+         * ```
+         * [
+         *   {
+         *     "character_id": 1000171,
+         *     "roles": [
+         *       "Director",
+         *       "Station_Manager"
+         *     ]
+         *   }
+         * ]
+         * ```
+         *
+         * @param {Integer} id The corporation id
+         * @param {String} accessToken The SSO access token of a member of the
+         *   corporation that has the personnel manager or any other grantable
+         *   role, used to authenticate the request
+         * @return {external:Promise} A Promise that resolves to the response of
+         *   the request
+         * @see https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_corporation_id_roles
+         * @esi_link CorporationApi.getCorporationsCorporationIdRoles
+         */
+        getRoles: function(id, accessToken) {
+            return newRequest(ESI.CorporationApi, 
+                              'getCorporationsCorporationIdRoles',
+                              [id], accessToken);
+        },
 
-    exports.getCorporationNames = function(ids) {
-        return newRequest(ESI.CorporationApi, 'getCorporationsNames', [ids]);
+        /**
+         * Get the names for a list of corporation ids from the ESI endpoint. 
+         * This makes an HTTP GET request to [`corporations/names/](https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_names).
+         * The request is returned as an asynchronous Promise that resolves to 
+         * an array parsed from the response JSON model. An example value looks 
+         * like:
+         *
+         * ```
+         * [
+         *   {
+         *     "corporation_id": 1000171,
+         *     "corporation_name": "Republic University"
+         *   }
+         * ]
+         * ```
+         *
+         * @param {Integer} id The corporation id
+         * @return {external:Promise} A Promise that resolves to the response of
+         *   the request
+         * @see https://esi.tech.ccp.is/latest/#!/Corporation/get_corporations_names
+         * @esi_link CorporationApi.getCorporationsNames
+         */
+        getNamesOf: function(ids) {
+            return newRequest(ESI.CorporationApi, 'getCorporationsNames', 
+                              [ids]);
+        }
     };
 
     return exports;
