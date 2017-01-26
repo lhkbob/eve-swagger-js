df9fa1aed3b5a933c5e010e36d5feb54e7bc5917
Michael Ludwig <lhkbob@gmail.com>
2017-01-26 13:41:38 -0600

    Implement a base class for functions that also have members.

---
+++ src/internal/ExtendableFunction.js
@@ -0,0 +1,21 @@
+/**
+ * ExtensibleFunction extends Function, allowing the addition of members and
+ * other methods to be added to the instance like a regular object while still
+ * being callable as a regular function. The default function that is executed
+ * when called as a function is passed to the constructor.
+ */
+class ExtensibleFunction extends Function {
+  /**
+   * Create an extensible function
+   *
+   * @param f The default action
+   * @returns {*} The function f with updated prototype
+   */
+  constructor(f) {
+    super();
+    Object.setPrototypeOf(f, new.target.prototype);
+    return f;
+  }
+}
+
+module.exports = ExtensibleFunction;
