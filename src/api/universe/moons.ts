a1b2c25776b932a8bc027a745d43ddd5f45f6ab5
Michael Ludwig <lhkbob@gmail.com>
2017-10-30 12:58:59 -0500

    Update stargates, moons, and planets to new module conventions

--- src/api/universe/moons.ts
+++ src/api/universe/moons.ts
@@ -1,62 +1,112 @@
-import { getNames } from '../..//internal/names';
 import { ESIAgent } from '../../internal/esi-agent';
-import { Responses, esi } from '../../internal/esi-types';
+import { Responses, esi } from '../../esi';
+
+import * as r from '../../internal/resource-api';
 
 /**
- * An api adapter that provides functions for accessing various details for a
- * moon specified by id, via functions in the
+ * The API specification for all variants that access information about an
+ * planetary interaction moon or multiple moons. This interface will
+ * not be used directly, but will be filtered through some mapper, such as
+ * {@link Async} or {@link Mapped} depending on what types of ids are being
+ * accessed. However, this allows for a concise and consistent specification for
+ * all variants: single, multiple, and all tasks.
+ *
+ * When mapped, each key defined in this interface becomes a function that
+ * returns a Promise resolving to the key's type, or a collection related to the
+ * key's type if multiple moons are being accessed at once.
+ *
+ * This is an API wrapper over the end points handling moons in the
  * [universe](https://esi.tech.ccp.is/latest/#/Universe) ESI endpoints.
  */
-export interface Moon {
-  /**
-   * @esi_example esi.moons(id).info()
-   *
-   * @returns Information on the specific moon
-   */
-  info() :Promise<Responses['get_universe_moons_moon_id']>;
-
-  /**
-   * @returns The moon's id
-   */
-  id() :Promise<number>;
+export interface MoonAPI {
+  details: Responses['get_universe_moons_moon_id'];
 }
 
 /**
- * An api adapter that provides functions for accessing moon information via the
- * [universe](https://esi.tech.ccp.is/latest/#/Universe) ESI end points.
+ * An api adapter for accessing various details of a single moon,
+ * specified by a provided id when the api is instantiated.
  */
-export interface Moons {
+export class Moon implements r.Async<MoonAPI>, r.SingleResource {
+  constructor(private agent: ESIAgent, private id: number | (() => Promise<number>)) { }
+
   /**
-   * Create a new Moon end point targeting the particular moon
-   * by `id`.
+   * @returns Information about the moon
+   */
+  details() {
+    return this.ids().then(id => getDetails(this.agent, id));
+  }
+
+  ids() {
+    if (typeof this.id === 'number') {
+      return Promise.resolve(this.id);
+    } else {
+      return this.id();
+    }
+  }
+}
+
+/**
+ * An api adapter for accessing various details of multiple moon ids,
+ * specified by a provided an array or set of ids when the api is instantiated.
+ */
+export class MappedMoons extends r.impl.SimpleMappedResource implements r.Mapped<MoonAPI> {
+  constructor(private agent: ESIAgent, ids: number[] | Set<number> | r.impl.IDSetProvider) {
+    super(ids);
+  }
+
+  /**
+   * @returns Moon details mapped by moon id
+   */
+  details() {
+    return this.getResource(id => getDetails(this.agent, id));
+  }
+}
+
+/**
+ * A functional interface for getting APIs for a specific moon or a
+ * known set of moon ids. There is currently no way to iterate over all
+ * moons in the game.
+ */
+export interface MoonAPIFactory {
+  /**
+   * Create a new moon api targeting the particular moon by `id`.
    *
    * @param id The moon id
-   * @returns A Moon API wrapper for the given id
+   * @returns An Moon API wrapper for the given id
    */
-  (id:number) :Moon;
+  (id: number): Moon;
+
+  /**
+   * Create a new moon api targeting the multiple moon ids. If an
+   * array is provided, duplicates are removed (although the input array is not
+   * modified).
+   *
+   * @param ids The moon ids
+   * @returns A MappedMoons API wrapper for the given ids
+   */
+  (ids: number[] | Set<number>): MappedMoons;
 }
 
 /**
- * Create a new {@link Moons} instance that uses the given `agent` to
+ * Create a new MoonAPIFactory instance that uses the given `agent` to
  * make its HTTP requests to the ESI interface.
  *
  * @param agent The agent making actual requests
- * @returns A Moons API instance
+ * @returns A MoonAPIFactory instance
  */
-export function makeMoons(agent:ESIAgent) :Moons {
-  return <Moons> <any> function(id:number) {
-    return new MoonImpl(agent, id);
-  }
+export function makeMoonAPIFactory(agent: ESIAgent): MoonAPIFactory {
+  return <MoonAPIFactory> function (ids: number | number[] | Set<number>) {
+    if (typeof ids === 'number') {
+      // Single id so single API
+      return new Moon(agent, ids);
+    } else {
+      // Set or array, so mapped API
+      return new MappedMoons(agent, ids);
+    }
+  };
 }
 
-class MoonImpl implements Moon {
-  constructor(private agent:ESIAgent, private id_:number) { }
-
-  info() {
-    return this.agent.request('get_universe_moons_moon_id', {path: {moon_id: this.id_}});
-  }
-
-  id() {
-    return Promise.resolve(this.id_);
-  }
+function getDetails(agent: ESIAgent, id: number) {
+  return agent.request('get_universe_moons_moon_id',
+      { path: { moon_id: id } });
 }
--- src/api/universe/planets.ts
+++ src/api/universe/planets.ts
@@ -1,56 +1,157 @@
 import { ESIAgent } from '../../internal/esi-agent';
-import { Responses } from '../../internal/esi-types';
+import { Responses, esi } from '../../esi';
+
+import * as r from '../../internal/resource-api';
+import { MappedMoons, Moon } from './moons';
 
 /**
- * An api adapter that provides functions for accessing various details for a
- * planet specified by id, via functions in the
+ * The API specification for all variants that access information about an
+ * planetary interaction planet or multiple planets. This interface will
+ * not be used directly, but will be filtered through some mapper, such as
+ * {@link Async} or {@link Mapped} depending on what types of ids are being
+ * accessed. However, this allows for a concise and consistent specification for
+ * all variants: single, multiple, and all tasks.
+ *
+ * When mapped, each key defined in this interface becomes a function that
+ * returns a Promise resolving to the key's type, or a collection related to the
+ * key's type if multiple planets are being accessed at once.
+ *
+ * This is an API wrapper over the end points handling planets in the
  * [universe](https://esi.tech.ccp.is/latest/#/Universe) ESI endpoints.
  */
-export interface Planet {
-  /**
-   * @esi_example esi.planets(id).info()
-   *
-   * @returns Information on all planets
-   */
-  info(): Promise<Responses['get_universe_planets_planet_id']>;
-
-  /**
-   * @returns The planet's id
-   */
-  id(): Promise<number>;
+export interface PlanetAPI {
+  details: Responses['get_universe_planets_planet_id'];
 }
 
 /**
- * An api adapter that provides functions for accessing planet information via
- * the [universe](https://esi.tech.ccp.is/latest/#/Universe) ESI end points.
+ * An api adapter for accessing various details of a single planet,
+ * specified by a provided id when the api is instantiated.
  */
-export interface Planets {
+export class Planet implements r.Async<PlanetAPI>, r.SingleResource {
+  private moons_: MappedMoons | undefined;
+
+  constructor(private agent: ESIAgent,
+      private id: number | (() => Promise<number>)) {
+  }
+
   /**
-   * Create a new Planet end point targeting the particular planet
-   * by `id`.
-   *
-   * @param id The planet id
-   * @returns A Planet API wrapper for the given id
+   * @returns Information about the planet
    */
-  (id: number): Planet;
+  details() {
+    return this.ids().then(id => getDetails(this.agent, id));
+  }
+
+  /**
+   * @esi_route ~get_universe_systems_system_id
+   *
+   * @param index The moon's index in the planet, NOT its ID, e.g. 0 represents
+   *    the planet's first moon.
+   * @returns A Moon instance representing the `index`th moon of the planet
+   */
+  moon(index: number) :Moon {
+    return new Moon(this.agent, () => this.ids()
+    .then(id => getMoonsForPlanet(this.agent, id))
+    .then(moons => moons[index]));
+  }
+
+  /**
+   * @esi_route ~get_universe_systems_system_id
+   *
+   * @returns A MappedMoons instance for all the moons of the planet
+   */
+  get moons() :MappedMoons {
+    if (this.moons_ === undefined) {
+      this.moons_ = new MappedMoons(this.agent,
+          () => this.ids().then(id => getMoonsForPlanet(this.agent, id)));
+    }
+    return this.moons_;
+  }
+
+  ids() {
+    if (typeof this.id === 'number') {
+      return Promise.resolve(this.id);
+    } else {
+      return this.id();
+    }
+  }
 }
 
-export function makePlanets(agent: ESIAgent): Planets {
-  return <Planets> <any> function (id: number) {
-    return new PlanetImpl(agent, id);
+/**
+ * An api adapter for accessing various details of multiple planet ids,
+ * specified by a provided an array or set of ids when the api is instantiated.
+ */
+export class MappedPlanets extends r.impl.SimpleMappedResource implements r.Mapped<PlanetAPI> {
+  constructor(private agent: ESIAgent, ids: number[] | Set<number> | r.impl.IDSetProvider) {
+    super(ids);
+  }
+
+  /**
+   * @returns Planet details mapped by planet id
+   */
+  details() {
+    return this.getResource(id => getDetails(this.agent, id));
+  }
+}
+
+/**
+ * A functional interface for getting APIs for a specific planet or a
+ * known set of planet ids. There is currently no way to iterate over all
+ * planets in the game.
+ */
+export interface PlanetAPIFactory {
+  /**
+   * Create a new planet api targeting the particular planet by `id`.
+   *
+   * @param id The planet id
+   * @returns An Planet API wrapper for the given id
+   */
+  (id: number): Planet;
+
+  /**
+   * Create a new planet api targeting the multiple planet ids. If an
+   * array is provided, duplicates are removed (although the input array is not
+   * modified).
+   *
+   * @param ids The planet ids
+   * @returns A MappedPlanets API wrapper for the given ids
+   */
+  (ids: number[] | Set<number>): MappedPlanets;
+}
+
+/**
+ * Create a new PlanetAPIFactory instance that uses the given `agent` to
+ * make its HTTP requests to the ESI interface.
+ *
+ * @param agent The agent making actual requests
+ * @returns A PlanetAPIFactory instance
+ */
+export function makePlanetAPIFactory(agent: ESIAgent): PlanetAPIFactory {
+  return <PlanetAPIFactory> function (ids: number | number[] | Set<number>) {
+    if (typeof ids === 'number') {
+      // Single id so single API
+      return new Planet(agent, ids);
+    } else {
+      // Set or array, so mapped API
+      return new MappedPlanets(agent, ids);
+    }
   };
 }
 
-class PlanetImpl {
-  constructor(private agent: ESIAgent, private id_: number) {
-  }
+function getDetails(agent: ESIAgent, id: number) {
+  return agent.request('get_universe_planets_planet_id',
+      { path: { planet_id: id } });
+}
 
-  info() {
-    return this.agent.request('get_universe_planets_planet_id',
-        { path: { planet_id: this.id_ } });
-  }
-
-  id() {
-    return Promise.resolve(this.id_);
-  }
+function getMoonsForPlanet(agent: ESIAgent, id: number) {
+  return getDetails(agent, id)
+  .then(details => agent.request('get_universe_systems_system_id',
+      { path: { system_id: details.system_id } }))
+  .then(system => {
+    for (let p of system.planets) {
+      if (p.planet_id === id) {
+        return p.moons || [];
+      }
+    }
+    return [];
+  });
 }
