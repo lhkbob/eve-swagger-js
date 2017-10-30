efb4319233c3b326c09e0742ad6e7e1e0a84fe43
Michael Ludwig <lhkbob@gmail.com>
2017-10-30 12:58:12 -0500

    Update races and bloodline modules to new conventions

--- src/api/universe/bloodlines.ts
+++ src/api/universe/bloodlines.ts
@@ -1,28 +1,144 @@
 import { ESIAgent } from '../../internal/esi-agent';
-import { Responses } from '../../internal/esi-types';
+import { esi } from '../../esi';
+
+import * as r from '../../internal/resource-api';
 
 /**
- * An api adapter over the end points handling bloodlines via functions in the
+ * The API specification for all variants that access information about an
+ * in-game bloodline or multiple types. This interface will not be used
+ * directly, but will be filtered through some mapper, such as {@link Async} or
+ * {@link Mapped} depending on what types of ids are being accessed. However,
+ * this allows for a concise and consistent specification for all variants:
+ * single, multiple, and all types.
+ *
+ * When mapped, each key defined in this interface becomes a function that
+ * returns a Promise resolving to the key's bloodline, or a collection related
+ * to the key's bloodline if multiple types are being accessed at once.
+ *
+ * This is an API wrapper over the end points handling types in the
  * [universe](https://esi.tech.ccp.is/latest/#/Universe) ESI endpoints.
  */
-export interface Bloodlines {
-  /**
-   * @esi_example esi.bloodlines()
-   *
-   * @returns Information on all the bloodlines defined in Eve
-   */
-  () :Promise<Responses['get_universe_bloodlines']>;
+export interface BloodlineAPI {
+  details: esi.universe.Bloodline;
 }
 
 /**
- * Create a new {@link Bloodlines} instance that uses the given `agent` to
+ * An api adapter for accessing various details of a single in-game bloodline,
+ * specified by a provided id when the api is instantiated.
+ */
+export class Bloodline extends r.impl.SimpleResource implements r.Async<BloodlineAPI> {
+  constructor(private agent: ESIAgent, id: number) {
+    super(id);
+  }
+
+  /**
+   * @esi_route ~get_universe_bloodlines
+   *
+   * @returns Information about the bloodline
+   */
+  details() {
+    return getBloodlines(this.agent)
+    .then(all => r.impl.filterArray(all, this.id_, r => r.bloodline_id));
+  }
+}
+
+/**
+ * An api adapter for accessing various details of multiple bloodline ids,
+ * specified by a provided an array or set of ids when the api is instantiated.
+ */
+export class MappedBloodlines extends r.impl.SimpleMappedResource implements r.Mapped<BloodlineAPI> {
+  constructor(private agent: ESIAgent, ids: number[] | Set<number>) {
+    super(ids);
+  }
+
+  /**
+   * @esi_route ~get_universe_bloodlines
+   *
+   * @returns Bloodline details mapped by bloodline id
+   */
+  details() {
+    return this.arrayIDs().then(ids => {
+      return getBloodlines(this.agent)
+      .then(all => r.impl.filterArrayToMap(all, ids, r => r.bloodline_id));
+    });
+  }
+}
+
+/**
+ * An api adapter for accessing various details about every bloodline in the
+ * game.
+ */
+export class AllBloodlines extends r.impl.SimpleIteratedResource<esi.universe.Bloodline> implements r.Iterated<BloodlineAPI> {
+  constructor(private agent: ESIAgent) {
+    super(r.impl.makeArrayStreamer(
+        () => agent.request('get_universe_bloodlines', undefined)), r => r.bloodline_id);
+  }
+
+  /**
+   * @esi_route get_universe_bloodlines
+   *
+   * @returns Iterator over details of all in-game types
+   */
+  details() {
+    return this.getPaginatedResource();
+  }
+}
+
+/**
+ * A functional interface for getting APIs for a specific bloodline, a
+ * known set of bloodline ids, or every bloodline in the game.
+ */
+export interface BloodlineAPIFactory {
+  /**
+   * Create a new bloodline api targeting every single bloodline in the game.
+   *
+   * @esi_route ids get_universe_types
+   *
+   * @returns An AllBloodlines API wrapper
+   */
+  (): AllBloodlines;
+
+  /**
+   * Create a new bloodline api targeting the particular bloodline by `id`.
+   *
+   * @param id The bloodline id
+   * @returns An Bloodline API wrapper for the given id
+   */
+  (id: number): Bloodline;
+
+  /**
+   * Create a new bloodline api targeting the multiple bloodline ids. If an
+   * array is provided, duplicates are removed (although the input array is not
+   * modified).
+   *
+   * @param ids The bloodline ids
+   * @returns A MappedBloodlines API wrapper for the given ids
+   */
+  (ids: number[] | Set<number>): MappedBloodlines;
+}
+
+/**
+ * Create a new BloodlineAPIFactory instance that uses the given `agent` to
  * make its HTTP requests to the ESI interface.
  *
  * @param agent The agent making actual requests
- * @returns An Bloodlines API instance
+ * @returns A BloodlineAPIFactory instance
  */
-export function makeBloodlines(agent: ESIAgent) :Bloodlines {
-  return function() {
-    return agent.request('get_universe_bloodlines', undefined);
-  }
+export function makeBloodlineAPIFactory(agent: ESIAgent): BloodlineAPIFactory {
+  return <BloodlineAPIFactory> function (ids: number | number[] | Set<number> | undefined) {
+    if (ids === undefined) {
+      // All types since no id
+      return new AllBloodlines(agent);
+    } else if (typeof ids === 'number') {
+      // Single id so single API
+      return new Bloodline(agent, ids);
+    } else {
+      // Set or array, so mapped API
+      return new MappedBloodlines(agent, ids);
+    }
+  };
+}
+
+function getBloodlines(agent: ESIAgent) {
+  return agent.request('get_universe_bloodlines', undefined);
 }
--- src/api/universe/races.ts
+++ src/api/universe/races.ts
@@ -1,28 +1,143 @@
 import { ESIAgent } from '../../internal/esi-agent';
-import { Responses } from '../../internal/esi-types';
+import { esi } from '../../esi';
+
+import * as r from '../../internal/resource-api';
 
 /**
- * An api adapter over the end points handling races via functions in the
+ * The API specification for all variants that access information about an
+ * in-game race or multiple types. This interface will not be used
+ * directly, but will be filtered through some mapper, such as {@link Async} or
+ * {@link Mapped} depending on what types of ids are being accessed. However,
+ * this allows for a concise and consistent specification for all variants:
+ * single, multiple, and all types.
+ *
+ * When mapped, each key defined in this interface becomes a function that
+ * returns a Promise resolving to the key's race, or a collection related to
+ * the key's race if multiple types are being accessed at once.
+ *
+ * This is an API wrapper over the end points handling types in the
  * [universe](https://esi.tech.ccp.is/latest/#/Universe) ESI endpoints.
  */
-export interface Races {
-  /**
-   * @esi_example esi.races()
-   *
-   * @returns Information on all races
-   */
-  (): Promise<Responses['get_universe_races']>;
+export interface RaceAPI {
+  details: esi.universe.Race;
 }
 
 /**
- * Create a new {@link Race} instance that uses the given `agent` to
+ * An api adapter for accessing various details of a single in-game race,
+ * specified by a provided id when the api is instantiated.
+ */
+export class Race extends r.impl.SimpleResource implements r.Async<RaceAPI> {
+  constructor(private agent: ESIAgent, id: number) {
+    super(id);
+  }
+
+  /**
+   * @esi_route ~get_universe_races
+   *
+   * @returns Information about the race
+   */
+  details() {
+    return getRaces(this.agent)
+    .then(all => r.impl.filterArray(all, this.id_, r => r.race_id));
+  }
+}
+
+/**
+ * An api adapter for accessing various details of multiple race ids,
+ * specified by a provided an array or set of ids when the api is instantiated.
+ */
+export class MappedRaces extends r.impl.SimpleMappedResource implements r.Mapped<RaceAPI> {
+  constructor(private agent: ESIAgent, ids: number[] | Set<number>) {
+    super(ids);
+  }
+
+  /**
+   * @esi_route ~get_universe_races
+   *
+   * @returns Race details mapped by race id
+   */
+  details() {
+    return this.arrayIDs().then(ids => {
+      return getRaces(this.agent)
+      .then(all => r.impl.filterArrayToMap(all, ids, r => r.race_id));
+    });
+  }
+}
+
+/**
+ * An api adapter for accessing various details about every race in the game.
+ */
+export class AllRaces extends r.impl.SimpleIteratedResource<esi.universe.Race> implements r.Iterated<RaceAPI> {
+  constructor(private agent: ESIAgent) {
+    super(r.impl.makeArrayStreamer(
+        () => agent.request('get_universe_races', undefined)), r => r.race_id);
+  }
+
+  /**
+   * @esi_route get_universe_races
+   *
+   * @returns Iterator over details of all in-game types
+   */
+  details() {
+    return this.getPaginatedResource();
+  }
+}
+
+/**
+ * A functional interface for getting APIs for a specific race, a
+ * known set of race ids, or every race in the game.
+ */
+export interface RaceAPIFactory {
+  /**
+   * Create a new race api targeting every single race in the game.
+   *
+   * @esi_route ids get_universe_types
+   *
+   * @returns An AllRaces API wrapper
+   */
+  (): AllRaces;
+
+  /**
+   * Create a new race api targeting the particular race by `id`.
+   *
+   * @param id The race id
+   * @returns An Race API wrapper for the given id
+   */
+  (id: number): Race;
+
+  /**
+   * Create a new race api targeting the multiple race ids. If an
+   * array is provided, duplicates are removed (although the input array is not
+   * modified).
+   *
+   * @param ids The race ids
+   * @returns A MappedRaces API wrapper for the given ids
+   */
+  (ids: number[] | Set<number>): MappedRaces;
+}
+
+/**
+ * Create a new RaceAPIFactory instance that uses the given `agent` to
  * make its HTTP requests to the ESI interface.
  *
  * @param agent The agent making actual requests
- * @returns An Races API instance
+ * @returns A RaceAPIFactory instance
  */
-export function makeRaces(agent: ESIAgent): Races {
-  return <Races> <any> function () {
-    return agent.request('get_universe_races', undefined);
+export function makeRaceAPIFactory(agent: ESIAgent): RaceAPIFactory {
+  return <RaceAPIFactory> function (ids: number | number[] | Set<number> | undefined) {
+    if (ids === undefined) {
+      // All types since no id
+      return new AllRaces(agent);
+    } else if (typeof ids === 'number') {
+      // Single id so single API
+      return new Race(agent, ids);
+    } else {
+      // Set or array, so mapped API
+      return new MappedRaces(agent, ids);
+    }
   };
+}
+
+function getRaces(agent: ESIAgent) {
+  return agent.request('get_universe_races', undefined);
 }
