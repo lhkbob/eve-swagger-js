833a9b49d44df05a9034b231f2c554b777318842
Michael Ludwig <lhkbob@gmail.com>
2017-10-30 19:46:56 -0500

    Update sovereignty to the new conventions

--- src/api/sovereignty.ts
+++ src/api/sovereignty.ts
@@ -1,58 +1,171 @@
 import { ESIAgent } from '../internal/esi-agent';
-import { Responses } from '../internal/esi-types';
+import { Responses, esi } from '../esi';
+
+import * as r from '../internal/resource-api';
 
 /**
- * An api adapter that provides functions for accessing the
+ * The API specification for all variants that access information about an
+ * in-game campaign or multiple campaigns. This interface will not be used
+ * directly, but will be filtered through some mapper, such as {@link Async} or
+ * {@link Mapped} depending on what types of ids are being accessed. However,
+ * this allows for a concise and consistent specification for all variants:
+ * single, multiple, and all campaigns.
+ *
+ * When mapped, each key defined in this interface becomes a function that
+ * returns a Promise resolving to the key's campaign, or a collection related
+ * to the key's campaign if multiple campaigns are being accessed at once.
+ *
+ * This is an API wrapper over the end points handling types in the
+ * [sovereignty](https://esi.tech.ccp.is/latest/#/Sovereignty) ESI endpoints.
+ */
+export interface CampaignAPI {
+  details: esi.sovereignty.Campaign;
+}
+
+/**
+ * An api adapter for accessing various details of a single in-game campaign,
+ * specified by a provided id when the api is instantiated.
+ */
+export class Campaign extends r.impl.SimpleResource implements r.Async<CampaignAPI> {
+  constructor(private agent: ESIAgent, id: number) {
+    super(id);
+  }
+
+  /**
+   * @esi_route ~get_universe_campaigns
+   *
+   * @returns Information about the campaign
+   */
+  details() {
+    return getCampaigns(this.agent)
+    .then(all => r.impl.filterArray(all, this.id_, r => r.campaign_id));
+  }
+}
+
+/**
+ * An api adapter for accessing various details of multiple campaign ids,
+ * specified by a provided an array or set of ids when the api is instantiated.
+ */
+export class MappedCampaigns extends r.impl.SimpleMappedResource implements r.Mapped<CampaignAPI> {
+  constructor(private agent: ESIAgent, ids: number[] | Set<number>) {
+    super(ids);
+  }
+
+  /**
+   * @esi_route ~get_universe_campaigns
+   *
+   * @returns Campaign details mapped by campaign id
+   */
+  details() {
+    return this.arrayIDs().then(ids => {
+      return getCampaigns(this.agent)
+      .then(all => r.impl.filterArrayToMap(all, ids, r => r.campaign_id));
+    });
+  }
+}
+
+/**
+ * An api adapter for accessing various details about every campaign in the
+ * game.
+ */
+export class IteratedCampaigns extends r.impl.SimpleIteratedResource<esi.sovereignty.Campaign> implements r.Iterated<CampaignAPI> {
+  constructor(private agent: ESIAgent) {
+    super(r.impl.makeArrayStreamer(
+        () => agent.request('get_sovereignty_campaigns', undefined)), r => r.campaign_id);
+  }
+
+  /**
+   * @esi_route get_universe_campaigns
+   *
+   * @returns Iterator over details of all in-game types
+   */
+  details() {
+    return this.getPaginatedResource();
+  }
+}
+
+/**
+ * A functional interface for getting APIs for a specific campaign, a
+ * known set of campaign ids, or every campaign in the game.
+ */
+export interface Campaigns {
+  /**
+   * Create a new campaign api targeting every single campaign in the game.
+   *
+   * @esi_route ids get_universe_types
+   *
+   * @returns An IteratedCampaigns API wrapper
+   */
+  (): IteratedCampaigns;
+
+  /**
+   * Create a new campaign api targeting the particular campaign by `id`.
+   *
+   * @param id The campaign id
+   * @returns An Campaign API wrapper for the given id
+   */
+  (id: number): Campaign;
+
+  /**
+   * Create a new campaign api targeting the multiple campaign ids. If an
+   * array is provided, duplicates are removed (although the input array is not
+   * modified).
+   *
+   * @param ids The campaign ids
+   * @returns A MappedCampaigns API wrapper for the given ids
+   */
+  (ids: number[] | Set<number>): MappedCampaigns;
+}
+
+/**
+ * A simple wrapper around functional interfaces for getting APIs for campaigns,
+ * sovereignty structures, and the sovereignty map. The sovereignty map is also
+ * accessible through the solar system apis. These utilize the
  * [sovereignty](https://esi.tech.ccp.is/latest/#/Sovereignty) ESI end points.
  */
-export interface Sovereignty {
+export class Sovereignty {
+  private campaigns_?:Campaigns;
 
-  /**
-   * @esi_example esi.sovereignty.campaigns()
-   *
-   * @return The current campaigns in Eve
-   */
-  campaigns(): Promise<Responses['get_sovereignty_campaigns']>;
+  constructor(private agent:ESIAgent) { }
 
-  /**
-   * @esi_example esi.sovereignty.structures()
-   *
-   * @return Details of the structures held in sovereignty
-   */
-  structures(): Promise<Responses['get_sovereignty_structures']>;
-
-  /**
-   * @esi_example esi.sovereignty.map()
-   *
-   * @return The details of the current sovereignty map
-   */
-  map(): Promise<Responses['get_sovereignty_map']>;
-}
-
-/**
- * Create a new {@link Sovereignty} instance that uses the given `agent` to
- * make its HTTP requests to the ESI interface.
- *
- * @param agent The agent making actual requests
- * @returns A Sovereignty API instance
- */
-export function makeSovereignty(agent: ESIAgent): Sovereignty {
-  return new SovereigntyImpl(agent);
-}
-
-class SovereigntyImpl implements Sovereignty {
-  constructor(private agent: ESIAgent) {
+  get campaigns() :Campaigns {
+    if (this.campaigns_ === undefined) {
+      this.campaigns_ = makeCampaigns(this.agent);
+    }
+    return this.campaigns_;
   }
 
-  campaigns() {
-    return this.agent.request('get_sovereignty_campaigns', undefined);
-  }
-
-  structures() {
+  /**
+   * @returns Information on all structures involved in sovereignty
+   *    conflicts
+   */
+  structures() : Promise<Responses['get_sovereignty_structures']> {
     return this.agent.request('get_sovereignty_structures', undefined);
   }
 
-  map() {
+  /**
+   * @returns The complete sovereignty map
+   */
+  map() :Promise<Responses['get_sovereignty_map']> {
     return this.agent.request('get_sovereignty_map', undefined);
   }
+}
+
+function makeCampaigns(agent: ESIAgent): Campaigns {
+  return <Campaigns> function (ids: number | number[] | Set<number> | undefined) {
+    if (ids === undefined) {
+      // All types since no id
+      return new IteratedCampaigns(agent);
+    } else if (typeof ids === 'number') {
+      // Single id so single API
+      return new Campaign(agent, ids);
+    } else {
+      // Set or array, so mapped API
+      return new MappedCampaigns(agent, ids);
+    }
+  };
+}
+
+function getCampaigns(agent: ESIAgent) {
+  return agent.request('get_sovereignty_campaigns', undefined);
 }
